b82c8a091b42b79b5b0bea31d75650d49706d062
Aviral <aviralgupta6@gmail.com>
2020-05-24 15:47:13 +0530

    W1End

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,24 @@ import { AppComponent } from './app.component';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishdetailComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
     FlexLayoutModule,
     MatListModule
   ],
